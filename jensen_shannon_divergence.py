6ce1b188e42163902a38adf089860b6e5f2eeaf3
rebecca0323 <rebeccaazhu@gmail.com>
2020-07-18 00:42:53 -0400

    feature selection w/ smote

--- jensen_shannon_divergence.py
+++ jensen_shannon_divergence.py
@@ -1,8 +1,21 @@
 import numpy as np
+import pandas as pd
 from scipy.spatial.distance import jensenshannon
+from imblearn.over_sampling import SMOTE
 import utils
 
 features, features_list, labels = utils.load_and_preprocess_data()
+
+isSMOTE = True
+if isSMOTE: 
+    os = SMOTE(random_state=0)
+    os_data_X,os_data_y=os.fit_sample(features, labels)
+    features = pd.DataFrame(data=os_data_X)
+    labels = pd.DataFrame(data=os_data_y)
+    print("length of oversampled data is ",len(os_data_X))
+    print(labels[0].value_counts())
+    features = np.array(features)
+    labels = np.array(labels).ravel()
 
 js_divergences = {}
 
@@ -47,6 +60,44 @@ sleep
 rest
 sound_sensitivity
 light_sensitivity
+helping_factors
+nausea_vomiting
+
+
+Using SMOTE:
+
+smoking
+no_exercise
+improper_lighting
+exercise
+sunlight
+exercise.1
+caffeine
+cheese_chocolate
+ovulation
+excess_sleep
+travel
+overeating
+drinking
+massage
+noise
+weather_temp
+irregular_meals
+emotional_changes
+odors
+other.1
+other
+fatigue
+menstruation
+less_sleep
+headache_day
+stress
+medicine
+light_sensitivity
+sleep
+sound_sensitivity
+total_triggers
+rest
 helping_factors
 nausea_vomiting
 
